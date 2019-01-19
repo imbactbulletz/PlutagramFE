c6bd7719049c14c474628957ad08ffb007a38f6b
imbactbulletz <cveticstefancpl@gmail.com>
2019-01-19 01:57:01 +0100

    Created a class with REST API calls and a TODO file.

---
+++ src/app/shared/rest-api-calls.ts
@@ -0,0 +1,13 @@
+export abstract class RESTAPI {
+  static baseUrl = 'http://192.168.0.14:8080';
+
+  // SIGN UP
+  public static getSignUpURL() {
+    return this.baseUrl + '/auth/register';
+  }
+
+  // SIGN IN
+  public static getSignInURL() {
+    return this.baseUrl + '/auth/login';
+  }
+}
