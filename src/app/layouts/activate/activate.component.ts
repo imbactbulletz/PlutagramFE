14f69247422818a2d2fc480e6585e77d525ed423
imbactbulletz <cveticstefancpl@gmail.com>
2019-01-21 03:10:17 +0100

    Implemented activation via url.

--- src/app/layouts/activate/activate.component.ts
+++ src/app/layouts/activate/activate.component.ts
@@ -1,20 +1,33 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {HttpClient, HttpParams} from '@angular/common/http';
 import {RESTAPI} from '../../shared/rest-api-calls';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-activate',
   templateUrl: './activate.component.html',
   styleUrls: ['./activate.component.scss']
 })
-export class ActivateComponent implements OnInit {
+export class ActivateComponent implements OnInit, AfterViewInit {
   token: string;
 
   @ViewChild('modal') modal: any;
+  @ViewChild('tokenInput') tokenInput: ElementRef;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private activeRoute: ActivatedRoute) { }
 
   ngOnInit() {
+
+    this.activeRoute.queryParams.subscribe( queryParams => {
+        const token = queryParams['key'];
+        if (token) {
+          this.token = token;
+        }
+      });
+  }
+
+  ngAfterViewInit() {
+    this.tokenInput.nativeElement.disabled = true;
   }
 
   activate() {
