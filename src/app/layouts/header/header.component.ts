04043aa73b5cef11838c9d9409acd68fce6ca653
imbactbulletz <cveticstefancpl@gmail.com>
2019-01-22 03:09:25 +0100

    Header nav items displayed according to user status.

--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  isLoggedIn() {
+    const token = localStorage.getItem('X-AUTH-TOKEN');
+
+    if (token) {
+      return true;
+    }
+
+    return false;
+  }
 }
